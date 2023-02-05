e3475d75415f84bdd6c76a4247c308a1cdb7ef57
Arnaud Tsombeng <arnaud.tsombeng@gmail.com>
2023-02-05 08:10:17 +0100

    update CICD pipeline

--- oilstubdata/oilandgasdata/views.py
+++ oilstubdata/oilandgasdata/views.py
@@ -33,12 +33,12 @@ class UploadOilStubDataView(View):
         return HttpResponseRedirect(reverse('home'))
 
 
-class SearchViewSet(APIView):
-    permission_classes = []
-
-    def post(self, request, *args, **kwargs):
-        serializer = SearchSerializer(data=request.data, context={'request': request})
-        if serializer.is_valid():
-            response = serializer.save()
-            return Response(response, status=status.HTTP_200_OK)
-        return Response({"message": 'Invalid search term'}, status=status.HTTP_400_BAD_REQUEST)
+# class SearchViewSet(APIView):
+#     permission_classes = []
+#
+#     def post(self, request, *args, **kwargs):
+#         serializer = SearchSerializer(data=request.data, context={'request': request})
+#         if serializer.is_valid():
+#             response = serializer.save()
+#             return Response(response, status=status.HTTP_200_OK)
+#         return Response({"message": 'Invalid search term'}, status=status.HTTP_400_BAD_REQUEST)

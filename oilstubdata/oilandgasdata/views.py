7abb8d0bbe48321202a3d3125eeeb6e3478b1128
Arnaud Tsombeng <arnaud.tsombeng@gmail.com>
2023-02-05 08:21:05 +0100

    update CICD pipeline

--- oilstubdata/oilandgasdata/views.py
+++ oilstubdata/oilandgasdata/views.py
@@ -2,13 +2,13 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.views import View
-# from rest_framework import status
-# from rest_framework.response import Response
-# from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from oilandgasdata.serializers import (
     UploadFileSerializer,
-    # SearchSerializer
+    SearchSerializer
 )
 
 
@@ -33,12 +33,12 @@ class UploadOilStubDataView(View):
         return HttpResponseRedirect(reverse('home'))
 
 
-# class SearchViewSet(APIView):
-#     permission_classes = []
-#
-#     def post(self, request, *args, **kwargs):
-#         serializer = SearchSerializer(data=request.data, context={'request': request})
-#         if serializer.is_valid():
-#             response = serializer.save()
-#             return Response(response, status=status.HTTP_200_OK)
-#         return Response({"message": 'Invalid search term'}, status=status.HTTP_400_BAD_REQUEST)
+class SearchViewSet(APIView):
+    permission_classes = []
+
+    def post(self, request, *args, **kwargs):
+        serializer = SearchSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            response = serializer.save()
+            return Response(response, status=status.HTTP_200_OK)
+        return Response({"message": 'Invalid search term'}, status=status.HTTP_400_BAD_REQUEST)

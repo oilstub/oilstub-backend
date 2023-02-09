7ad07c13e633580f38f432df8a664993e5f0d5a0
Arnaud Tsombeng <arnaud.tsombeng@gmail.com>
2023-02-09 19:58:28 +0100

    integrate with elastic search server

--- oilstubdata/search/serializers.py
+++ oilstubdata/search/serializers.py
@@ -1,5 +1,19 @@
-from rest_framework import serializers
+from django_elasticsearch_dsl_drf.serializers import DocumentSerializer
+
+from oilandgasdata.documents import GpCountyDocument
 
 
-class SearchSerializer(serializers.Serializer):
-    search_term = serializers.CharField(max_length=200)
+class GpCountyDocumentSerializer(DocumentSerializer):
+    class Meta:
+        document = GpCountyDocument
+        fields = (
+            'district_name',
+            'district_no',
+            'county_name',
+            'county_no',
+            'created_at',
+            'county_fips_code',
+            'on_shore_flag',
+            'onshore_assc_cnty_flag',
+        )
+

fc659a4bbaa89de9150408e325ff6e28fc6c23c0
Arnaud Tsombeng <arnaud.tsombeng@gmail.com>
2023-02-10 13:20:38 +0100

    add search filter endpoints for GP county

--- oilstubdata/search/urls.py
+++ oilstubdata/search/urls.py
@@ -8,13 +8,4 @@ router.register(
     r'api/v1/gp-county-search', basename='gp-county-search',
     viewset=views.GpCountySearchView
 )
-router.register(
-    r'api/v1/gp-county-filter', basename='gp-county-filter',
-    viewset=views.GpCountyFilterView
-)
-router.register(
-    r'api/v1/gp-county-autocomplete', basename='gp-county-autocomplete',
-    viewset=views.GpCountyCompletionView
-)
-
 urlpatterns = router.urls
